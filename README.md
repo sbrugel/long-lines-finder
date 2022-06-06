5c6bb9aba2407d1db20325cbabd59e5e2f4f737b
sbrugel <sbrugel62@gmail.com>
2022-06-06 09:57:11 -0400

    feat: toggle whitespace removal

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # long-lines-finder
 Reads through a user specified text file and finds lines that are over a number of characters the user inputs.
 
+## Features
+**Optional Whitespace Removal (Added 06/06/2022)**
+
+When reading through a file, you can enable/disable if leading and trailing whitespace is included when counting characters:
+
+![no_strip](https://i.imgur.com/yeInWzO.png)
+
+![strip](https://i.imgur.com/FUsL9k1.png)
+
 ## Notes
 For the file input, this should be an absolute path only, with no quotes around the path name.
--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-maxChars = int(input('Enter max amount of acceptable characters: '))
+max_chars = int(input('Enter max amount of acceptable characters: '))
 fname = input('Enter file to check: ')
+
+strip_lines = 'x'
+while not strip_lines == 'y' and not strip_lines == 'n':
+    strip_lines = input('Strip whitespace when checking lines? (y/n) ')
 
 f = open(fname, "r")
 lines = f.readlines()
 
 count = 0
 for line in lines:
+    curr_line = line
+    if strip_lines == 'y':
+        curr_line = line.strip()
     count += 1
-    if (len(line) > maxChars):
-        print('Line ' + str(count) + ' is more than ' + str(maxChars) + ' characters long')
-        print('Line contents are: ' + line + '\n')
+    if (len(curr_line) > max_chars):
+        print('Line ' + str(count) + ' is more than ' + str(max_chars) + ' characters long')
+        print('Line contents are: ' + curr_line + '\n')
